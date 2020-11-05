37d26faeb0471703f575430bfedd5f6b79b45628
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-11-05 12:10:38 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,9 +7,7 @@ export ANSI_RESET="\e[0m"
 echo -e "\n $ANSI_YELLOW *** testing docker run - buildpack-deps *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Display current directory: $ANSI_RESET"
-docker run -i --name some-buildpack quay.io/ibmz/buildpack-deps:bionic pwd
-docker exec -i some-buildpack "apt-get update -y | grep s390x"
-docker stop some-buildpack && docker rm some-build-pack
+docker run -i --rm --name some-buildpack quay.io/ibmz/buildpack-deps:bionic "apt-get update -y | grep s390x"
 docker rmi quay.io/ibmz/buildpack-deps:bionic
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
