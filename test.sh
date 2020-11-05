5d2c3b71db35f7b3f5e0c989cc6ba237ccd97828
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-11-05 12:26:48 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** testing docker run - buildpack-deps *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW access to ubuntu-bionic package repositories? (this image is used as a base for building light-weight ubuntu environments): $ANSI_RESET"
+docker run -i --rm --name some-buildpack quay.io/ibmz/buildpack-deps:buster apt-get update -y | grep s390x
+docker rmi quay.io/ibmz/buildpack-deps:buster
+
+echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
